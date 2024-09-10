3532a94699aff2d581ea6ed2863e2bcfb37e03eb
HabibAsyrof <habibasyrof2006@icloud.com>
2024-09-10 22:02:52 +0700

    capek

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ const navbarNav = document.querySelector
 ('.navbar-nav')
 
 document.querySelector('#hamburger-menu')
-onclick = () => {
-    navbarNav.classList.toggle('active')
-}
-
-document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active')
-    }
-});
+.onclick=()=>{
+    navbarNav.classList.toggle('active');
+};
